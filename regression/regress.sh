8c60e336516bf82b03ce51c5ce94489c03c5523e
Rahul Jadhav <nyrahul@gmail.com>
2017-08-28 04:04:54 +0000

    added more regressiong TCs. Handled SIGCHLD in airline.

--- regression/regress.sh
+++ regression/regress.sh
@@ -6,10 +6,37 @@
 DIR=`dirname "$0"`
 REG_DIR=`realpath "$DIR"`
 WF_CMD="$REG_DIR/../invoke_whitefield.sh"
+WFSH="$REG_DIR/../scripts/wfshell"
 TC_LOG="$REG_DIR/testcase_report.log"
 rm $TC_LOG 2>/dev/null
 tc_cnt=0
 tc_fail=0
+
+get_status_print()
+{
+	RED='\033[0;31m'
+	GREEN='\033[0;32m'
+	NC='\033[0m' # No Color
+	[[ $1 -eq 0 ]] && tc_status="${GREEN}PASS${NC}" && return
+	tc_status="${RED}FAIL${NC}"
+}
+
+pre_tc_exec()
+{
+	$WFSH stop_whitefield >/dev/null
+	unset TC_MSG
+}
+
+post_tc_exec()
+{
+	$WFSH stop_whitefield >/dev/null
+	unset TC_MSG
+}
+
+tc_set_msg()
+{
+	TC_MSG="$*"
+}
 
 exec_testcase()
 {
@@ -22,13 +49,17 @@ exec_testcase()
 	source "$1"
 
 	#execute testcase
+	pre_tc_exec
 	testcase >>$TC_LOG
+	post_tc_exec
 
 	#print testcase status
 	tc_res=$?
 	((tc_cnt++))
 	[[ $tc_res -ne 0 ]] && ((tc_fail++))
-	echo -en "$tc_cnt $tcname res:$tc_res tot_fail:$tc_fail\n" | tee -a $TC_LOG
+	get_status_print $tc_res
+	echo -en "[$tc_cnt] [$tcname] [$tc_status] [tot_fail:$tc_fail]\n" | tee -a $TC_LOG
+	[[ "$TC_MSG" != "" ]] && echo -en "tc_msg:$TC_MSG\n"
 }
 
 regress_dir()
@@ -43,14 +74,21 @@ regress_cfg()
 {
 	[[ -d "$1" ]] && regress_dir "$1" && return
 	while read line; do 
+		line=`echo $line`	#trim spaces
+		[[ "${line}" == "" ]] && continue;
 		[[ "${line:0:1}" == "#" ]] && continue;
-		line="$REG_DIR/$line"
-		[[ -d "$line" ]] && regress_dir "$line" && continue
-		[[ -f "$line" ]] && exec_testcase "$line" && continue
-		echo "Invalid line:<$line> in <$1> ... ignoring..."
+		path="$REG_DIR/$line"
+		if [ -d "$path" ]; then
+			regress_dir "$path"
+		elif [ -f "$path" ]; then 
+			exec_testcase "$path"
+		else
+			echo "Invalid line:<$line> in <$1> ... ignoring..."
+		fi
 	done < "$1"
 }
 
 regress_cfg "$1"
 echo "check $TC_LOG for execution log..."
-[[ $tc_fail -ne 0 ]] && exit 1
+[[ $tc_fail -ne 0 ]] && echo "$tc_fail testcases failed!!" && exit 1
+exit 0
