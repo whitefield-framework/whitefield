5dd4c602b71da826257bc627615fad3a0063f406
Rahul Jadhav <nyrahul@gmail.com>
2020-05-03 14:29:29 +0530

    configurable br node id. BR_ID

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR=`dirname $0`
-watch -n2 $DIR/whitefield_status.sh
+watch -n2 $DIR/whitefield_status.sh $*
 
 # Have commented below after getting problem with "read: read resource temporarily unavailable"
 #WATCH_PID=$!
